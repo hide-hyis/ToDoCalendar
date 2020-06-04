4adad79868fe09b45d4d87bb0a915e471fb74ff2
hide-hyis <ishii_pma@yahoo.co.jp>
2020-06-04 12:57:21 +0900

    設定画面カテゴリー表示・変更実装

--- ToDoCalendar/Controller/SettingViewController.swift
+++ ToDoCalendar/Controller/SettingViewController.swift
@@ -12,15 +12,17 @@ import JGProgressHUD
 
 class SettingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate, TableViewCellDelegate {
     
-//    @IBOutlet weak var tableView: UITableView!
-    var customView: UIView?                  // 白背景の表示用View
     var navHeight: CGFloat?                 // navBarの高さ
     var hud = JGProgressHUD(style: .dark)   // アラートメッセージ用
     var calendarVC: ViewController?        // 遷移元のViewController
     var tapUIView: UIView?                 // カレンダーエリア
+    let headerView = UIView()                 // tableView header
+    let doneBtn = UIButton()              // tableView header 完了ボタン
     @IBOutlet weak var tableView: UITableView!
-    var categoryArray = [Category]()
+    var categoryArray = [Category]()      // 表示するカテゴリー名の配列
+    var categoryIdArray = [String]()        // tableViewに表示しているカテゴリーID
     
+    @IBOutlet weak var customView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +42,7 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         tapUIView = UIView(frame: CGRect(x: 250, y: 0, width: self.view.frame.width - 250, height: self.view.frame.height) )
         self.view.addSubview(self.tapUIView!)
         
-        //自作セルをテーブルビューに登録する。
-        let customXib = UINib(nibName:"TableViewCell", bundle:nil)
-        tableView.register(customXib, forCellReuseIdentifier: "inputCell")
-//        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "inputCell")
+        self.tableView.isHidden = true
     }
     
     // MARK: UIGestureRecognizerDelegate
@@ -60,32 +59,31 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5 //categoryArray.count
+        return categoryArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "inputCell", for: indexPath) as! TableViewCell
-        cell.delegate = self
-//        cell.textField.isEnabled = true
+        let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
+        cell.delegate = self
         if categoryArray.count > 0{
-//            cell.textLabel?.text = categoryArray[indexPath.row].name!
+            cell.textField.text = categoryArray[indexPath.row].name!
         }
         return cell
     }
+    
+    // MARK: UITextFieldDelegate
+    // textfield 入力直後に呼ばれる
     func textFieldDidEndEditing(cell: TableViewCell, value: String) -> () {
         let indexPath = tableView.indexPathForRow(at: cell.convert(cell.bounds.origin, to: tableView))
         categoryArray[indexPath!.row].name! = value
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
     }
     
-    
-    // MARK: - Handler
-
+    // MARK: EVENT ACTION
     @objc func back(){
         
         self.dismiss(animated: true)
@@ -107,9 +105,36 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         }
     }
     @objc func categoryBtn(){
-        print("カテゴリーボタンタップ")
+        if self.tableView.isHidden{
+           self.tableView.isHidden = false
+            self.headerView.isHidden = false
+            doneBtn.isHidden = false
+        }else{
+            self.tableView.isHidden = true
+            self.headerView.isHidden = true
+            doneBtn.isHidden = true
+        }
     }
     
+    @objc func editCategory(){
+        guard let currentUid = Auth.auth().currentUser?.uid else {return}
+        let updateTime = Date().timeIntervalSince1970
+        
+        var i = 0
+        for category in self.categoryArray {
+            
+            let values = ["name": category.name,
+                          "createdTime": category.createdTime,
+                          "updatedTime": updateTime] as [String: Any]
+            
+            var categoryId = categoryIdArray[i]
+            CATEGORIES_REF.child(currentUid).child(categoryId).updateChildValues(values)
+            i += 1
+        }
+        print("カテゴリーの編集完了処理")
+        self.dismiss(animated: true)
+    }
+    // MARK: - Handler
     // 成功用JGProgress
     func jgprogressSuccess(str: String){
         hud.textLabel.text = str
@@ -123,12 +148,13 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         self.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
         
         // 土台となるUIView
-        self.customView = UIView(frame: CGRect(x: 0, y: 0, width: 220, height: self.view.frame.height - 250 ) )
-        self.customView!.backgroundColor = .white
-        view.addSubview(customView!)
+//        self.customView = UIView(frame: CGRect(x: 0, y: 0, width: 220, height: self.view.frame.height ) )
+//        self.customView!.backgroundColor = .white
+//        
+//        view.addSubview(customView!)
         
         // 右側の線
-        let sideLine = UIView(frame: CGRect(x: 220, y: 0, width: 2, height: self.view.frame.height - 250) )
+        let sideLine = UIView(frame: CGRect(x: 220, y: 0, width: 2, height: self.view.frame.height) )
         sideLine.backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230, alpha: 0.9)
         customView?.addSubview(sideLine)
         
@@ -161,7 +187,7 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         
         //　ログアウトラベル
         let logoutLabel = UILabel()
-        logoutLabel.frame = CGRect(x: 50, y: 90, width: 120, height: 30)
+        logoutLabel.frame = CGRect(x: 50, y: 80, width: 120, height: 30)
         logoutLabel.backgroundColor = .clear
         logoutLabel.text = "ログアウト"
         logoutLabel.font = UIFont.boldSystemFont(ofSize: 18)
@@ -169,7 +195,7 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         
         // ログアウトボタン
         let logoutButton  = UIButton()
-        logoutButton.frame = CGRect(x: 170, y: 90, width: 25, height: 25)
+        logoutButton.frame = CGRect(x: 170, y: 80, width: 25, height: 25)
         let logoutButtonImage = UIImage(named: "logout")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
         logoutButton.setImage(logoutButtonImage, for: .normal)
         logoutButton.tintColor = .black
@@ -179,7 +205,7 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         
         //　カテゴリーラベル
         let categoryLabel = UILabel()
-        categoryLabel.frame = CGRect(x: 50, y: 150, width: 120, height: 30)
+        categoryLabel.frame = CGRect(x: 50, y: 120, width: 120, height: 30)
         categoryLabel.backgroundColor = .clear
         categoryLabel.text = "カテゴリー"
         categoryLabel.font = UIFont.boldSystemFont(ofSize: 18)
@@ -187,13 +213,30 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         
         // カテゴリーボタン
         let categoryButton  = UIButton()
-        categoryButton.frame = CGRect(x: 170, y: 150, width: 25, height: 25)
+        categoryButton.frame = CGRect(x: 170, y: 120, width: 25, height: 25)
         let categoryButtonImage = UIImage(named: "edit-icon")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
         categoryButton.setImage(categoryButtonImage, for: .normal)
         categoryButton.tintColor = .black
         categoryButton.addTarget(self, action: #selector(categoryBtn), for: UIControl.Event.touchUpInside)
         self.customView!.addSubview(categoryButton)
         self.customView!.bringSubviewToFront(categoryButton)
+        
+        // tableView header
+        headerView.frame = CGRect(x: 0, y: 160, width: self.tableView.bounds.width, height: 60)
+        headerView.backgroundColor = UIColor.rgb(red: 225, green: 225, blue: 225, alpha: 0.9)
+        self.customView?.addSubview(headerView)
+        self.customView!.bringSubviewToFront(headerView)
+        
+        self.doneBtn.frame = CGRect(x:160 , y: 160, width: 50, height: 30)
+        self.doneBtn.setTitle("完了", for: .normal)
+        self.doneBtn.setTitleColor(.black, for: .normal)
+        self.doneBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        self.doneBtn.addTarget(self, action: #selector(editCategory), for: UIControl.Event.touchUpInside)
+        self.customView?.addSubview(self.doneBtn)
+        self.customView!.bringSubviewToFront(doneBtn)
+        headerView.isHidden = true
+        doneBtn.isHidden = true
+        
     }
     
     // MARK: API
@@ -202,9 +245,11 @@ class SettingViewController: UIViewController, UITableViewDataSource, UITableVie
         
         CATEGORIES_REF.child(currentUid).observe(.childAdded) { (snapshot) in
             guard let dictionary = snapshot.value as? Dictionary<String, AnyObject> else {return}
+            let categoryId = snapshot.key as String
             let category = Category(dictionary: dictionary)
             
             self.categoryArray.append(category)
+            self.categoryIdArray.append(categoryId)
             self.tableView.reloadData()
         }
     }
